e02a8f01126aa704720763cd5c664c4c3fb9f67b
Raph40 <rafajoselopes@gmail.com>
2024-12-04 15:26:12 +0000

    precario em obras com ligação do gridview

---
+++ lib/precario.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class precario extends StatelessWidget {
+  const precario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: precarioPage(),
+    );
+  }
+}
+
+class precarioPage extends StatelessWidget {
+  const precarioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                blurRadius: 6,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Text(
+              "Questionário",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
